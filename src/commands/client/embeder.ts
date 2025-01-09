413b6595b9a6c9b17dd3f53a3a56ca298770e924
angelurrutdev <angeljurrut@gmail.com>
2025-01-09 19:59:33 +0000

    fix embed errors

--- src/commands/client/embeder.ts
+++ src/commands/client/embeder.ts
@@ -5,14 +5,26 @@ export const command: Command = {
     data: new SlashCommandBuilder()
         .setName('embed')
         .setDescription('Envía un mensaje con un embed.')
-        .setDMPermission(false)
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .addChannelOption((option) => option.setName('canal').setDescription('Canal de texto').setRequired(true).addChannelTypes(0))
-        .addStringOption((option) => option.setName('mensaje').setDescription('Mensaje del embed. Se usará como descripción si no se especifica una.').setRequired(true))
-        .addStringOption((option) => option.setName('titulo').setDescription('Título'))
-        .addStringOption((option) => option.setName('descripcion').setDescription('Descripción'))
-        .addStringOption((option) => option.setName('color').setDescription('Color del embed')
-            .setChoices(
+        .setDMPermission(false) // No disponible en MD
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator) // Requiere permisos de administrador
+        .addChannelOption((option) => option
+            .setName('canal')
+            .setDescription('Canal de texto donde se enviará el embed.')
+            .setRequired(true)
+            .addChannelTypes(0)) // Solo canales de texto
+        .addStringOption((option) => option
+            .setName('mensaje')
+            .setDescription('Mensaje que se usará como descripción si no se especifica una descripción.'))
+        .addStringOption((option) => option
+            .setName('titulo')
+            .setDescription('Título del embed.'))
+        .addStringOption((option) => option
+            .setName('descripcion')
+            .setDescription('Descripción del embed. Soporta Markdown.')) // Se indica el soporte de Markdown
+        .addStringOption((option) => option
+            .setName('color')
+            .setDescription('Color del embed.')
+            .setChoices( // Opciones de color predefinidas
                 { name: 'Gris', value: 'Greyple' },
                 { name: 'Verde', value: 'Green' },
                 { name: 'Rojo', value: 'Red' },
@@ -23,15 +35,21 @@ export const command: Command = {
                 { name: 'Naranja', value: 'Orange' },
                 { name: 'Random', value: 'Random' },
             ))
-        .addStringOption((option) => option.setName('timestamp').setDescription('Añadir hora')
-            .setChoices(
-                { name: 'Si', value: 'yes' },
-                { name: 'No', value: 'no' },
-            ))
-        .addStringOption((option) => option.setName('autor').setDescription('Nombre del autor'))
-        .addStringOption((option) => option.setName('footer').setDescription('Texto del pie'))
-        .addAttachmentOption((option) => option.setName('thumbnail').setDescription('Miniatura'))
-        .addAttachmentOption((option) => option.setName('image').setDescription('Imagen')),
+        .addBooleanOption((option) => option
+            .setName('timestamp')
+            .setDescription('Añadir la marca de tiempo actual al embed.'))
+        .addStringOption((option) => option
+            .setName('autor')
+            .setDescription('Nombre del autor del embed.'))
+        .addStringOption((option) => option
+            .setName('footer')
+            .setDescription('Texto del pie de página del embed.'))
+        .addAttachmentOption((option) => option
+            .setName('thumbnail')
+            .setDescription('Imagen para la miniatura del embed.'))
+        .addAttachmentOption((option) => option
+            .setName('image')
+            .setDescription('Imagen principal del embed.')),
 
     async execute(client, interaction: ChatInputCommandInteraction) {
         const embed = new EmbedBuilder();
@@ -39,37 +57,39 @@ export const command: Command = {
 
         const channel = interaction.options.getChannel('canal');
         const mensaje = interaction.options.getString('mensaje');
-
-        if (!channel || !(channel instanceof TextChannel || channel instanceof NewsChannel || channel instanceof ThreadChannel)) {
-            return interaction.reply({ embeds: [errorEmbed.setDescription('Debes seleccionar un canal de texto válido.')], ephemeral: true });
-        }
-
-        let description = (interaction.options.getString('descripcion') ?? mensaje)!
-
-        if (description.trim() === "") {
-            return interaction.reply({ embeds: [errorEmbed.setDescription('La descripción no puede estar vacía.')], ephemeral: true });
-        }
-
-        const title = interaction.options.getString('titulo');
-        const timestamp = interaction.options.getString('timestamp');
+        const titulo = interaction.options.getString('titulo');
+        let descripcion = interaction.options.getString('descripcion') ?? mensaje ?? ""; // Prioriza 'descripcion', luego 'mensaje', y si ambos son nulos, usa una cadena vacía.
+        const timestamp = interaction.options.getBoolean('timestamp');
         let color = interaction.options.getString('color') as ColorResolvable | undefined;
         const autor = interaction.options.getString('autor');
         const footer = interaction.options.getString('footer');
         const thumbnail = interaction.options.getAttachment('thumbnail');
         const image = interaction.options.getAttachment('image');
 
-        if (title) embed.setTitle(title);
-        if (description.length > 4096) return interaction.reply({ embeds: [errorEmbed.setDescription('La descripción excede los 4096 caracteres.')], ephemeral: true });
-        embed.setDescription(description);
+        // Validación del canal
+        if (!channel || !(channel instanceof TextChannel || channel instanceof NewsChannel || channel instanceof ThreadChannel)) {
+            return interaction.reply({ embeds: [errorEmbed.setDescription('Debes seleccionar un canal de texto válido.')], ephemeral: true });
+        }
+
+        // Validación de descripción vacía
+        if (descripcion.trim() === "") {
+            return interaction.reply({ embeds: [errorEmbed.setDescription('La descripción no puede estar vacía.')], ephemeral: true });
+        }
+
+        // Configuración del embed
+        if (titulo) embed.setTitle(titulo);
+        if (descripcion.length > 4096) return interaction.reply({ embeds: [errorEmbed.setDescription('La descripción excede los 4096 caracteres.')], ephemeral: true });
+        embed.setDescription(descripcion); // Aquí se aplica el Markdown
         if (thumbnail) embed.setThumbnail(thumbnail.url);
         if (image) embed.setImage(image.url);
-        if (timestamp === 'yes') embed.setTimestamp();
+        if (timestamp) embed.setTimestamp();
         if (autor) embed.setAuthor({ name: autor });
         if (footer) embed.setFooter({ text: footer });
 
+        // Configuración del color
         if (color) {
             if (color === 'Random') {
-                color = Math.floor(Math.random() * 16777215).toString(16) as ColorResolvable;
+                color = Math.floor(Math.random() * 16777215).toString(16) as ColorResolvable; // Genera un color hexadecimal aleatorio
             }
             embed.setColor(color);
         }
